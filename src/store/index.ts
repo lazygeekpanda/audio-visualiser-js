68e577c892ebbfd1f8e59381af86a7842a0ecfcd
Žymantas Venclauskas <30467983+lazygeekpanda@users.noreply.github.com>
2022-07-20 16:12:27 +0300

    Updated audio links

--- src/store/index.ts
+++ src/store/index.ts
@@ -10,7 +10,8 @@ const store: Store = {
       artist: 'Hoober',
       trackName: 'Higher',
       url: 'Hoober_Higher.mp3',
-      prodUrl: 'https://audio.kodeo.lt/audio_files/Hoober_Higher.mp3',
+      // prodUrl: 'https://audio-exp.kodeo.lt/audio_files/Hoober_Higher.mp3',
+      prodUrl: require('assets/audio/Hoober_Higher.mp3'),
       attributionText: `Song: Hoober - Higher (feat. Vanessa Campagna) [NCS Release]
       Music provided by NoCopyrightSounds
       Free Download/Stream: http://ncs.io/HHigher
@@ -20,7 +21,8 @@ const store: Store = {
       artist: 'Abandoned',
       trackName: 'Out of the Grave',
       url: 'Abandoned_Out_of_the_Grave.mp3',
-      prodUrl: 'https://audio.kodeo.lt/audio_files/Abandoned_Out_of_the_Grave.mp3',
+      prodUrl: require('assets/audio/Abandoned_Out_of_the_Grave.mp3'),
+      // prodUrl: 'https://audio-exp.kodeo.lt/audio_files/Abandoned_Out_of_the_Grave.mp3',
       attributionText: `Song: Abandoned - Out of the Grave (Feat. ENROSA) [NCS Release]
       Music provided by NoCopyrightSounds
       Free Download/Stream: http://ncs.io/OutOfTheGrave
@@ -30,7 +32,8 @@ const store: Store = {
       artist: 'Egzod, Maestro Chives, Neoni',
       trackName: 'Royalty',
       url: 'Egzod_Royalty.mp3',
-      prodUrl: 'https://audio.kodeo.lt/audio_files/Egzod_Royalty.mp3.mp3',
+      prodUrl: require('assets/audio/Egzod_Royalty.mp3'),
+      // prodUrl: 'https://audio-exp.kodeo.lt/audio_files/Egzod_Royalty.mp3.mp3',
       attributionText: `Song: Egzod, Maestro Chives, Neoni - Royalty [NCS Release]
       Music provided by NoCopyrightSounds
       Free Download/Stream: http://ncs.io/Royalty
@@ -40,7 +43,8 @@ const store: Store = {
       artist: 'Abstrakt',
       trackName: 'Nobody Else',
       url: 'Abstrakt_Nobody_Else.mp3',
-      prodUrl: 'https://audio.kodeo.lt/audio_files/Abstrakt_Nobody_Else.mp3',
+      prodUrl: require('assets/audio/Abstrakt_Nobody_Else.mp3'),
+      // prodUrl: 'https://audio-exp.kodeo.lt/audio_files/Abstrakt_Nobody_Else.mp3',
       attributionText: `Song: Abstrakt - Nobody Else [NCS Release]
       Music provided by NoCopyrightSounds
       Free Download/Stream: http://NCS.io/NobodyElse
@@ -50,7 +54,8 @@ const store: Store = {
       artist: 'QR',
       trackName: 'XXI',
       url: 'QR_XXI.mp3',
-      prodUrl: 'https://audio.kodeo.lt/audio_files/QR_XXI.mp3',
+      prodUrl: require('assets/audio/QR_XXI.mp3'),
+      // prodUrl: 'https://audio-exp.kodeo.lt/audio_files/QR_XXI.mp3',
       attributionText: `Song: QR - XXI [NCS Release]
       Music provided by NoCopyrightSounds
       Free Download/Stream: http://NCS.io/XXI
@@ -58,8 +63,5 @@ const store: Store = {
     }
   ],
 }
-
-// TODO: Add tracks:
-// Lost Identities - Moments
 
 export default store
